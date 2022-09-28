4a57e937a1f157c804d15688a8ed9dd218476d3d
houzifahabbo <h-hebbo@hotmail.com>
2022-09-28 22:54:20 +0300

    t

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:coffee/view/auth/login/view/login_view.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginView(),
-      }
-    );
+        theme: ThemeData(
+          elevatedButtonTheme:  ElevatedButtonThemeData(
+            style:ElevatedButton.styleFrom(
+                backgroundColor: Color(0xff4aa366),
+              //TODO public height and width
+              padding: EdgeInsets.symmetric(vertical:20, horizontal: 168),
+            ),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LoginView(),
+        }
+        );
   }
 }
--- lib/view/auth/login/view/login_view.dart
+++ lib/view/auth/login/view/login_view.dart
@@ -4,18 +4,102 @@ import 'package:flutter/material.dart';
 ///The design should be on this page. Widgets should be split.
 ///I have to reach events like click by saying model.blabla
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   LoginView({Key? key}) : super(key: key);
+
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
   final LoginViewModel model = LoginViewModel();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:  Column(
-        children: [
-          const Text('Login view'),
-        ],
-      )
-    );
+    double phoneWidth = MediaQuery.of(context).size.width;
+    double phoneHeight = MediaQuery.of(context).size.height;
+    return Stack(
+      children:[
+        Scaffold(
+            backgroundColor: Color(0xfff7f8fa),
+            body:  SafeArea(
+              child:
+              Padding(
+                padding: EdgeInsets.only(left: phoneWidth*0.05,top: phoneHeight*0.2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/starbucks.png'),
+                      Padding(
+                        padding:EdgeInsets.only(top: phoneHeight*0.02),
+                        child: const Text(
+                          'Welcome back!',
+                          style: TextStyle(
+                              fontSize: 35,
+                              color: Color(0xFF2e2d38),
+                              fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:EdgeInsets.only(top: phoneHeight*0.02),
+                        child: const Text(
+                          'Lorem ipsum dolor sit amet, consectetur\nadipiscing elit sed do eiusmod tempor',
+                          style: TextStyle(
+                              color: Color(0xff4d5661),
+                              fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:EdgeInsets.only(top: phoneHeight*0.03),
+                        child: const TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              focusColor: Colors.red,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                              labelText: 'Email',
+                            )
+                        ),
+                      ),
+                      Padding(
+                        padding:EdgeInsets.only(top: phoneHeight*0.02),
+                        child: const TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                            )),
+                      ),
+                     Padding(
+                          padding:EdgeInsets.only(top: phoneHeight*0.05 ,left: phoneWidth*0.55),
+                          child: const Text(
+                            'Forgot your password?',
+                            style: TextStyle(
+                                color: Color(0xff4d5661)
+                            ),
+                          ),
+                        ),
+
+                      Padding(
+                        padding:EdgeInsets.only(top: phoneHeight*0.05),
+                        child: ElevatedButton(
+                          onPressed: (){},
+                          style:  const ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll<Color>(Color(0xff4aa366)),
+                          ),
+                          child: Text('Login'),
+                        ),
+
+                      )
+                    ]),
+              ),
+            )
+        ),
+        Image.asset('assets/background.png'),
+    ]);
   }
 }
